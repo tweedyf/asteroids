68e1b7527c783d9cb439fcb28cbe656cd4315d23
Tweedy F <tweedy@gmail.com>
2025-05-14 22:06:06 +0200

    cleaning up commented lines

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 100
-#	print("Starting Asteroids!")
-#	print(f"Screen width: {SCREEN_WIDTH}")
-#	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
@@ -28,9 +29,6 @@ def main():
 	# Create objects
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	asteroidfield = AsteroidField()
-
-#	drawable.add(player)
-#	updatable.add(player)
 
 	while True:
 		for event in pygame.event.get():
